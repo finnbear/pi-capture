f5fa6dcaee85a7891a1778bec7ecde8e2c2bfae5
finnbear <finnbearone@gmail.com>
2017-03-17 17:01:03 -0700

    Motion detection.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
+import time
 import picamera
 from SimpleCV import *
 
+motion_threshold = 2.0
 image_path = 'image.png'
+gallery_path = './gallery'
 
 camera = picamera.PiCamera()
 
@@ -25,6 +28,11 @@ camera.vflip = False
 camera.zoom = (0.2,0,1,1)
 
 def main():
+	if not os.path.exists(gallery_path):
+		os.makedirs(gallery_path)
+
+	prev_image = None
+
 	while disk() < 90:
 		# Capture an image
 		camera.capture(image_path, format='png')
@@ -32,7 +40,16 @@ def main():
 		# Process the image
 		image = Image(image_path)
 		image = image.rotate(-90)
-		image.save(image_path)
+
+		# Detect motion
+		if not prev_image == None:
+			motion_image, motion_amount = detectMotion(image, prev_image)
+
+			if motion_amount > motion_threshold:
+				image.save(image_path)
+
+		# Swap image into prev_image
+		prev_image = image
 	camera.close()
 
 def disk():
@@ -44,5 +61,15 @@ def disk():
 		if i==2:
 			return int(line.split()[1:5][3][:-1])
 
-print disk()
+def detectMotion(motion_image_1, motion_image_2):
+	motion_image_1 = motion_image_1.toGray()
+	motion_image_2 = motion_image_2.toGray()
+	motion_image_diff = motion_image_1 - motion_image_2
+	motion_image_diff = motion_image_diff.binarize(25)
+	motion_image_diff = motion_image_diff.invert()
+	motion_matrix_diff = motion_image_diff.getNumpy()
+	motion_average = motion_matrix_diff.mean()
 
+	return motion_image_diff, motion_average
+
+main()
